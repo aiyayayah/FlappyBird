c8ad7f119ba78bf4e79c074f6ec168e1a839e3b2
Kah Yong <cky734@gmail.com>
2025-04-09 21:53:10 +0800

    add scoring system

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -4,6 +4,7 @@ public class PlayerScript : MonoBehaviour
 {
     public float jumpHeight = 0f;
     public Rigidbody2D rb;
+    public int score = 0;
    void Start()
     {
         
@@ -17,12 +18,21 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) //built in function
+    private void OnCollisionEnter2D(Collision2D collision) //built in function -- collide
     {
         if (collision.gameObject.tag == "Pillar")
         {
             Debug.Log("Game Over");
             Time.timeScale = 0; //stop the game
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //built in function  -- trigger
+    {
+        if(collision.gameObject.tag == "ADDScore")
+        {
+            score++;
+            Debug.Log(score);
         }
     }
 }
