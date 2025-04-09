eca338e45f23a99eee3a00eb58249cd0ce392fbd
Kah Yong <cky734@gmail.com>
2025-04-09 17:39:56 +0800

    make the 4 pillars continuous spawn

--- Assets/Script/CreateObstacle.cs
+++ Assets/Script/CreateObstacle.cs
@@ -4,14 +4,29 @@ public class CreateObstacle : MonoBehaviour
 {
     public GameObject[] obstacles;
     public Transform spawnPoint;
+    public bool doCreate = true;
+    public float nextPillarCreationTime = 2.0f;
     void Start()
     {
-        Instantiate(obstacles[3], spawnPoint.position, spawnPoint.rotation);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(doCreate)
+        {
+            int randomIndex = Random.Range(0, obstacles.Length);
+
+            Instantiate(obstacles[randomIndex], spawnPoint.position, spawnPoint.rotation);
+
+            Invoke("ResetDoCreate", nextPillarCreationTime); //delay time after nextPillarCreated
+            //doCreate = false; //prevent immediate respawn
+        }
+    }
+
+    public void ResetDoCreate()
+    {
+        doCreate = true;   
     }
 }
