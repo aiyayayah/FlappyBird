816d5cc3d293e6986d05b0df993646531da53afc
Kah Yong <cky734@gmail.com>
2025-04-11 12:22:17 +0800

    add gameOver and restartGame UI logic

--- Assets/Script/StartButton.cs
+++ Assets/Script/StartButton.cs
@@ -4,19 +4,11 @@ public class StartButton : MonoBehaviour
 {
     public GameManager gm;
     public GameObject startButtonUI;
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
 
     public void StartGame()
     {
         gm.GameStart = true; 
         startButtonUI.SetActive(false);
+        Time.timeScale = 1;
     }
 }
